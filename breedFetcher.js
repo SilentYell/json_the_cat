6e11d3af3d79bf502bae8a5e669b38c99f13ef33
Nik Hixson <nikilobe@gmail.com>
2024-10-25 16:20:32 -0700

    adding require('fs') and a variable to store the url. began work on needle.get

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,11 @@
 const needle = require('needle');
+const fs = require('fs');
 
+
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+
+needle.get(url, (error, response) => {
+  if (error) {
+    fs.writeFile('./catbreed.txt');
+  }
+});
