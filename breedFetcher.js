d06456aa07227628f40f723de2f11f1a3c3e613d
Nik Hixson <nikilobe@gmail.com>
2024-10-26 13:58:22 -0700

    added the rest of the if and else statements to work with the consistent url but will now modify to take in a user searching a specific breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,11 +7,14 @@ const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
 needle.get(url, (error, response) => {
   if (error) {
     console.log('Error:', error);
-    console.log('status code:', response && response.statusCode);
+    console.log('Status code:', response && response.statusCode);
     return;
   }
 
   if (response.statusCode === 200) {
-    fs.writeFile(JSON.stringify('./catBreedData.json'));
+    fs.writeFile('./catBreedData.json', JSON.stringify(response.body, null, 2), (err) => {
+      if (err) console.log('Write error:', err);
+      else console.log('Successfully wrote to catBreedData.json');
+    });
   }
 });
