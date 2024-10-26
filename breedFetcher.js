f7a9b69f02d301d625773087dbfb87805d03f113
Nik Hixson <nikilobe@gmail.com>
2024-10-26 14:21:17 -0700

    refactored into fetchBreedDescription function using callbacks and exported it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,31 @@
 const needle = require('needle');
 const fs = require('fs');
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+// const breedName = process.argv[2];
 
-needle.get(url, (error, response) => {
-  if (error) {
-    console.log('Error:', error);
-    console.log('Status code:', response && response.statusCode);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (response.statusCode === 200) {
-    const data = response.body[0];
-
-    if (data) {
-      console.log(data.description);
-      fs.writeFile('./catBreedData.json', JSON.stringify(response.body, null, 2), (err) => {
-        if (err) console.log('Write error:', err);
-        else console.log('Successfully wrote to catBreedData.json');
-      });
-    } else {
-      console.log('Breed not found.');
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  
+  needle.get(url, (error, response) => {
+    if (error) {
+      callback(`Failed to get breed description: ${error}`, null);
+      return;
     }
-  }
-});
+    
+    if (response.statusCode === 200) {
+      const data = response.body[0];
+      
+      if (data) {
+        callback(null, data.description);
+      } else {
+        callback('Breed not found', null);
+      }
+
+    } else {
+      callback(`Failed with status code: ${response.statusCode}`, null);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
