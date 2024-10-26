ab2f02661856bdea777c0964bba4825fa41a96fa
Nik Hixson <nikilobe@gmail.com>
2024-10-26 13:47:28 -0700

    adding some if statements to handle error and response

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,6 +6,12 @@ const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
 
 needle.get(url, (error, response) => {
   if (error) {
-    fs.writeFile('./catbreed.txt');
+    console.log('Error:', error);
+    console.log('status code:', response && response.statusCode);
+    return;
+  }
+
+  if (response.statusCode === 200) {
+    fs.writeFile(JSON.stringify('./catBreedData.json'));
   }
 });
